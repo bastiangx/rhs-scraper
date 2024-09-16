0fdf2165039c5a4319a6183ae62c09b404bb0292
bastian <bastiangx@pm.me>
2024-09-16 15:55:16 +0100

    feat: robust checks, more docs

--- src/scraper/scraper.go
+++ src/scraper/scraper.go
@@ -1,16 +1,9 @@
-// package scraper implements a simple web scraper to get events from the Royal Holloway Students' Union Events Calendar.
-// It uses the Colly library to scrape the events from the website.
-// The events are stored in a JSON file in the user's cache directory.
-// The scraper checks if the data is recent (within 12 hours) and uses the cached data if it is.
-// The user can force scrape the data if they want to.
-// The user can also specify the number of events to scrape (min 2, max 25).
-// The scraped data will be handed to Bubbles package later for TUI display.
+// Package scraper implements a web scraper to retrieve events from the RHUL students' union events' calendar.
+// uses Colly library for scraping, json file for caching and stops users from spamming the dest server.
 package scraper
 
 import (
-	"bufio"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -22,9 +15,8 @@ import (
 	"github.com/gocolly/colly"
 )
 
-// Event struct stores the event details
-// Title, Date, Location, Category, Description
-// can be marshalled to JSON
+// Event struct contains event card info
+// can have default values for each field
 type Event struct {
 	Title       string `json:"title"`
 	Date        string `json:"date"`
@@ -33,319 +25,246 @@ type Event struct {
 	Description string `json:"description"`
 }
 
-// CachedData struct stores last timestamp
+// CachedData struct holds the timestamp of the last scrape and the list of events
 type CachedData struct {
 	LastScraped time.Time `json:"last_scraped"`
 	Events      []Event   `json:"events"`
 }
 
-func main() {
-	// cmd flags
-	// -f can be used to force scrape even if cache exists
-	forceScrape := flag.Bool("f", false, "force scraping")
+// ScrapeEvents func scrapes specified number of events
+// returns a slice of Event and an Err if the scraping fails
+func ScrapeEvents(numEvents int) ([]Event, error) {
+	// validate num of events
+	if numEvents < 2 {
+		numEvents = 2
+	} else if numEvents > 25 {
+		numEvents = 25
+	} // min max can be changed later
 
-	// -n can be used to specify number of events to scrape
-	numEvents := flag.Int("n", 10, "Number of events to scrape (min 2, max 25).")
-	flag.Parse()
-
-	// validate number of events
-	if *numEvents < 2 {
-		*numEvents = 2
-	} else if *numEvents > 25 {
-		*numEvents = 25
-	}
-
-	if *forceScrape || shouldScrape() {
-		fmt.Println("Scraping --> ")
-
-		scrapeEvents(*numEvents)
-	} else {
-		fmt.Println("recent data found!")
-		fmt.Println("Do you want to Force scrape? (y/n): ")
-
-		// scan user input
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		response := strings.ToLower(scanner.Text())
-
-		if response == "y" || response == "f" {
-			scrapeEvents(*numEvents)
-		} else {
-			fmt.Println("using cached data.")
-			loadCachedEvents()
-		}
-	}
-}
-
-////////////////////////////////////////////
-//////////////// SCRAPING //////////////////
-////////////////////////////////////////////
-
-// scrapeEvents function scrapes the events from the SU Events Calendar
-// uses colly collector
-// using OnHTML since the data is in HTML
-func scrapeEvents(numEvents int) {
-	// store the events in a map
-	eventsMap := make(map[string]*Event)
-
-	// store the events in a slice
-	var events []Event
-
-	// Counter
-	eventCount := 0
-
-	// Main collector
+	// main collector
 	c := colly.NewCollector()
 
-	// title of the event
-  // the targeted string is in the class msl_event_name via css selector
-	c.OnHTML(".msl_event_name", func(e *colly.HTMLElement) {
+	// slice of events
+	var events []Event
+	eventCount := 0
+
+	// OnHTML handler func for each event container
+	// returns the title, date, location, category and description
+	// uses GoQuery selector for parsing the html, i think
+	// used becuase of the structure of the page -> only css selectors and classes & no js or ajax rendering
+	c.OnHTML(".msl_eventlist .event_item", func(e *colly.HTMLElement) {
 		if eventCount >= numEvents {
+			// stop scraping
+			e.Request.Abort()
 			return
 		}
 
-		// set the title of the event
-		title := e.Text
-
-		// check if the event already exists
-		event, found := eventsMap[title]
-		if !found {
-			// if not, create a new event
-			event = &Event{}
-			eventsMap[title] = event
-		}
-		event.Title = title
-		fmt.Println("Event title:", event.Title)
-	})
-
-	// Scrape event date
-	// all c.OnHTML functions are similar
-	// onlu the class name changes / css selector
-	// we also check the parent node to get the possible missing data
-	c.OnHTML(".msl_event_time", func(e *colly.HTMLElement) {
-		if eventCount >= numEvents {
+		// extract title
+		title := strings.TrimSpace(e.ChildText("a.msl_event_name"))
+		if title == "" {
+			log.Println("!! event with an empty title.")
 			return
 		}
-		// get the parent node and find class name
-		title := e.DOM.Parent().Find(".msl_event_name").Text()
-		// get the event from the map
-		if event, found := eventsMap[title]; found {
-			// set the date
-			event.Date = e.Text
-			fmt.Println("Event date:", event.Date)
-		}
-	})
 
-	// event description
-	c.OnHTML(".msl_event_description", func(e *colly.HTMLElement) {
-		if eventCount >= numEvents {
-			return
-		}
-		title := e.DOM.Parent().Find(".msl_event_name").Text()
-		if event, found := eventsMap[title]; found {
-			event.Description = e.Text
-			fmt.Println("Event description:", event.Description)
-		}
-	})
+		// extract date
+		// dd is the tag and msl_event_time is the class
+		// from the html structure
+		date := strings.TrimSpace(e.ChildText("dd.msl_event_time"))
 
-	// event location
-	c.OnHTML(".msl_event_location", func(e *colly.HTMLElement) {
-		if eventCount >= numEvents {
-			return
-		}
-		title := e.DOM.Parent().Find(".msl_event_name").Text()
-		if event, found := eventsMap[title]; found {
-			event.Location = e.Text
-			fmt.Println("Event location:", event.Location)
-		}
-	})
+		// extract location
+		location := strings.TrimSpace(e.ChildText("dd.msl_event_location"))
 
-	// event category
-	// this one has some filtering
-	// for spammed categories
-	c.OnHTML(".msl_event_types", func(e *colly.HTMLElement) {
-		if eventCount >= numEvents {
-			return
-		}
-		parentEvent := e.DOM.Parent().Parent()
-		title := parentEvent.Find(".msl_event_name").Text()
+		// extract desc
+		description := strings.TrimSpace(e.ChildText("dd.msl_event_description"))
 
-		if event, found := eventsMap[title]; found {
-			validCategoryFound := false
+		// extract category
+		// also checks if its a spammed cat -> "free", etc
 
-			e.ForEach("a", func(i int, el *colly.HTMLElement) {
-				categoryText := el.Text
-
-				// skip "Free" category
-				if categoryText == "Free" || categoryText == "free" {
-					return
-				}
-
-				// assign the first valid category found
-				event.Category = categoryText
-				validCategoryFound = true
-				fmt.Println("Event category:", event.Category)
-				return
-			})
-
-			// Default category if no valid cat found
-			if !validCategoryFound {
-				event.Category = "Uncategorized"
-				fmt.Println("no valid category found, assigning 'Uncategorized'")
+		// default category
+		category := "uncategorized"
+		var categories []string
+		// for each category
+		e.ForEach("dd.msl_event_types a", func(_ int, el *colly.HTMLElement) {
+			categoryText := strings.TrimSpace(el.Text)
+			// check if its not a spam or empty
+			if strings.ToLower(categoryText) != "free" && categoryText != "" {
+				categories = append(categories, categoryText)
 			}
-		} else {
-			fmt.Println("Warning: Category found but no matching event title!")
+		})
+		if len(categories) > 0 {
+			category = strings.Join(categories, ", ")
 		}
+
+		// Log extracted details
+		// temporary log
+		// delete later
+		// log.Printf("Extracted Event %d:\n", eventCount+1)
+		// log.Printf("Title: %s\n  Date: %s\n  Location: %s\n  Category: %s\n  Description: %s\n",
+			// title, date, location, category, description)
+
+		// append the valid events
+		event := Event{
+			Title:       title,
+			Date:        date,
+			Location:    location,
+			Category:    category,
+			Description: description,
+		}
+		events = append(events, event)
+		eventCount++
+
+		// log.Printf("appended event %d: %s\n", eventCount, title)
 	})
 
-	// handle request
-	// temporary Println, will be bubbles later
+	// OnRequest func requests the url
+	// can be shown to the user
+	// can be pure BubbleTea UI later
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting right now...", r.URL)
+		log.Printf("Visiting %s\n", r.URL.String())
 	})
 
-	////////////////////////////////////////////
-	////////////// AFTER SCRAPING //////////////
-	////////////////////////////////////////////
-
-	// store the events in a JSON
-	c.OnScraped(func(r *colly.Response) {
-		for _, event := range eventsMap {
-			if eventCount >= numEvents {
-				break
-			}
-			events = append(events, *event)
-			eventCount++
-		}
-
-		// cachedData strcut
-		// appends events to the slice
-		// has a timestamp
-		// time.now is later converted to human readable format
-		cachedData := CachedData{
-			LastScraped: time.Now(),
-			Events:      events,
-		}
-
-		fmt.Println("Found events: ", events)
-
-		cacheDir := getCacheDir()
-		jsonFilePath := filepath.Join(cacheDir, "events.json")
-
-		// save all to JSON with timestamp
-		file, err := os.Create(jsonFilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-
-		// Init json encoder
-		encoder := json.NewEncoder(file)
-		// indent for readability
-		encoder.SetIndent("", "  ")
-
-		if err := encoder.Encode(cachedData); err != nil {
-			log.Fatal("!!! error encoding events to JSON: ", err)
-		}
-
-		fmt.Println("Events saved to", jsonFilePath)
+	// OnResponse func logs the status code from the server
+	// can be shown to the user if Err
+	c.OnResponse(func(r *colly.Response) {
+		log.Printf("received response with code: %d\n", r.StatusCode)
 	})
 
-	// start scraping
-	// visit the events Calendar
-	c.Visit("https://su.rhul.ac.uk/events/calendar/")
+	// OnError func logs the error if any
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("Error visiting %s: %v\n", r.Request.URL.String(), err)
+		// TODO: add normal print or show to user if error occurs
+	})
+
+	// visti func send a get request to the URL
+	// at the end of collector since it needs
+	// to compile or setup all the handlers first
+	err := c.Visit("https://su.rhul.ac.uk/events/calendar/")
+	if err != nil {
+		return nil, fmt.Errorf("failed to visit events page: %w", err)
+	}
+
+	// wait for the collector to finish
+	err = saveToCache(events)
+	if err != nil {
+		return nil, fmt.Errorf("failed to save events to cache: %w", err)
+	}
+
+	log.Printf("scraping succesful. %d events found and saved.\n", len(events))
+	return events, nil
 }
 
-////////////////////////////////////////////
-//////////////// CHECK CACHE ///////////////
-////////////////////////////////////////////
+// saveToCache func encodes the current scraped events
+// along with the current timestamp to a JSON cache file.
+// files' path are os defined, via UserCacheDir func
+func saveToCache(events []Event) error {
+	cachedData := CachedData{
+		LastScraped: time.Now(),
+		Events:      events,
+	}
 
-// shouldScrape function checks if the data is recent
-// if the data is older than 12 hours, it returns true
-// retrieves the timestamp from the json file
-func shouldScrape() bool {
 	cacheDir := getCacheDir()
 	jsonFilePath := filepath.Join(cacheDir, "events.json")
 
-	// load the cache
+	// default os based
+	file, err := os.Create(jsonFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to create cache file: %w", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	if err := encoder.Encode(cachedData); err != nil {
+		return fmt.Errorf("failed to encode events to JSON: %w", err)
+	}
+
+	log.Printf("new events saved to %s\n", jsonFilePath)
+	return nil
+}
+
+// ShouldScrape func determines whether a new scrape is necessary based on timestamp
+// returns true if the cache is older than 12 hours or doesnt exist
+func ShouldScrape() bool {
+	cacheDir := getCacheDir()
+	jsonFilePath := filepath.Join(cacheDir, "events.json")
+
 	file, err := os.Open(jsonFilePath)
 	if err != nil {
-		fmt.Println("no recent data found, scraping...")
+		log.Println("no recent data found, scraping new events...")
 		return true
 	}
 	defer file.Close()
 
+	// decode the cache file
 	var cachedData CachedData
-	// read and decode the json
 	decoder := json.NewDecoder(file)
 	if err := decoder.Decode(&cachedData); err != nil {
-		fmt.Println("!!! error reading cache, scraping...")
+		log.Println("!error reading cache, scraping...")
 		return true
 	}
 
-	// if last scraped time is within 12 hours
-	// uses humanized time format
-	if time.Since(cachedData.LastScraped) < 12*time.Hour {
-		fmt.Printf(
-			"*** Using cached data. Last scraped: %s\n",
-			humanize.Time(cachedData.LastScraped),
-		)
+	// TODO: feat: later, maybe user can specify a soft time limit in settings
+
+	// timestamp check
+	// for now, var is 12 hours
+	var timeLimit time.Duration = 12
+	if time.Since(cachedData.LastScraped) < timeLimit*time.Hour {
+		log.Printf("last scraped %s ago, no need to scrape new events.\n",
+			humanize.Time(cachedData.LastScraped))
 		return false
 	}
 
-	// if older than 12 hours, scrape
+	log.Printf("last scraped more than %d hours ago, scraping new events...\n", 12)
 	return true
 }
 
-////////////////////////////////////////////
-//////////////// CACHE DIR /////////////////
-////////////////////////////////////////////
-
-// getCacheDir function gets the cache directory
-// creates the directory if it doesn't exist
-// for mac and windows, it uses the default cache directory
-// for linux, it is /.cache/rhs-scraper under the home directory
-func getCacheDir() string {
-	// os method to get default cache directory
-	cacheDir, err := os.UserCacheDir()
-	if err != nil {
-		log.Fatal("!!! error getting cache directory:", err)
-	}
-
-	// append custom dir
-	appCacheDir := filepath.Join(cacheDir, "rhs-scraper")
-
-	// create dir if it doesnt exist
-	if _, err := os.Stat(appCacheDir); os.IsNotExist(err) {
-		err := os.Mkdir(appCacheDir, 0755) // permission: rwxr-xr-x
-		if err != nil {
-			log.Fatal("!!! error creating cache directory:", err)
-		}
-	}
-	return appCacheDir
-}
-
-////////////////////////////////////////////
-//////////////// LOAD CACHE ////////////////
-////////////////////////////////////////////
-
-// loadCachedEvents function prints the events from json
-// uses json decoder
-func loadCachedEvents() {
+// LoadCachedEvents func loads events from the cache
+// returns a slice of Event and an error if loading fails
+func LoadCachedEvents() ([]Event, error) {
 	cacheDir := getCacheDir()
 	jsonFilePath := filepath.Join(cacheDir, "events.json")
 
 	file, err := os.Open(jsonFilePath)
 	if err != nil {
-		log.Fatal("!!! error loading cache:", err)
+		return nil, fmt.Errorf("!error opening cache file: %w", err)
 	}
 	defer file.Close()
 
 	var cachedData CachedData
 	decoder := json.NewDecoder(file)
 	if err := decoder.Decode(&cachedData); err != nil {
-		log.Fatal("!!! error decoding cache:", err)
+		return nil, fmt.Errorf("!error decoding cache file: %w", err)
 	}
 
-	fmt.Println("loaded cached events:", cachedData.Events)
+	log.Printf("--> Loaded %d events from cache.\n", len(cachedData.Events))
+	return cachedData.Events, nil
+}
+
+// getCacheDir func retrieves the cache directory path
+// also appends the app name to path
+// returns the path as a string
+func getCacheDir() string {
+	// UserCacheDir, os method gets the default cache directory
+	// in linux, its ~/.cache/...
+	// in win, its %APPDATA%/...
+	// in mac, its ~/Library/Caches/...
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		log.Fatalf("!!error getting user cache directory: %v", err)
+	}
+
+	appCacheDir := filepath.Join(cacheDir, "rhs-scraper")
+
+	if _, err := os.Stat(appCacheDir); os.IsNotExist(err) {
+		err := os.Mkdir(appCacheDir, 0755) // permission: rwxr-xr-x
+		if err != nil {
+			log.Fatalf("!!!error creating cache directory: %v", err)
+			log.Fatalf("make sure you have the correct .cache directory")
+			log.Fatalf("visit the git page to see the correct cache directory")
+		}
+	}
+
+	// return path as string
+	return appCacheDir
 }
